2aa09793eb2ed3f107f7d3b2ca7300d3392fb73e
James Butcher <jamesbutcher167@gmail.com>
2021-12-26 17:49:54 +0000

    Add implementation for DisplayManager

---
+++ src/displaymanager.cpp
@@ -0,0 +1,56 @@
+#include "display.hpp"
+#include <raylib.h>
+
+DisplayManager::DisplayManager(
+    const int screenWidth,
+    const int screenHeight,
+    const std::string title,
+    const int targetFPS,
+    const std::string profile
+){
+  InitWindow(screenWidth, screenHeight, title.data());
+  SetTargetFPS(targetFPS);
+
+  this->profile = profile;
+  this->screenWidth = screenWidth;
+  this->screenHeight = screenHeight;
+}
+
+void DisplayManager::switchProfile(std::string profile){
+  if (!profiles.contains(profile))
+    profiles[profile] = {};
+
+  this->profile = profile;
+
+  this->profileRef = &profiles[profile];
+}
+
+void DisplayManager::attachShape(Object* shape){
+  profileRef->push_back(shape);
+}
+
+void DisplayManager::removeShape(Object* shape){
+  for (size_t i = 0; i < profileRef->size(); i++){
+    if (profileRef->at(i) == shape){
+      profileRef->erase(profileRef->begin()+i);
+      return;
+    }
+  }
+}
+
+void DisplayManager::drawShape() const{
+  for (Object* i : *profileRef)
+    i->draw();
+}
+
+void DisplayManager::closeScreen() const{
+  CloseWindow();
+}
+
+void DisplayManager::clear(){
+  profileRef->clear();
+}
+
+std::vector<Object*> DisplayManager::exposeShapes(std::string profile) const{
+  return profiles.at(profile);
+}
